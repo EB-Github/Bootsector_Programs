763848438d98876e3790f09d70045bda55524eb3
elishaaidanbrown1994@gmail.com <elishaaidanbrown1994@gmail.com>
2017-04-10 15:35:05 -0500

    added print and io

--- bootsector.asm
+++ bootsector.asm
@@ -4,19 +4,40 @@ jmp init
 
 init:
 	mov bp,0x8000 ;stack
-	mov sp, bp	
-
+	mov sp, bp
+	
 	mov bx, STRING ;get string
 	call print_string_16	;print string in bx
+	call newline_16
 
-	jmp end
+	mov bx,STRING2
+	call print_string_16
+	call newline_16
+	jmp read_command
+
+read_command:
+	mov ah, 00h
+	int 0x16
+	mov dl, al
+	cmp dl, 13
+	je run_command 
+	call print_char_16
+
 	
+	jmp read_command
+
+run_command:
+	call newline_16
+	mov bx, STRING2
+	call print_string_16
+	jmp read_command
 end:
 	jmp $	; loop forever
 
 %include 'print_realmode.asm'
+%include 'input_realmode.asm'
 
 STRING db 'Hello world',0
-
+STRING2 db 'Waiting for keyboard input..',0
 times 510-($-$$) db 0 ;pad boot sector
 dw 0xaa55 ;signature word at end of boot sector
--- print_realmode.asm
+++ print_realmode.asm
@@ -1,22 +1,32 @@
 print_string_16:
-	push dx
+	push dx 
 	push bx
-	mov dl, [bx]
-	cmp dl, 0
+	mov dl, [bx] 		; move string to dl
+	cmp dl, 0    		; check for end of string
 	je return
 
-	call print_char_16
-	inc bx
+	call print_char_16 	;print character
+	inc bx		   	;next character
 	call print_string_16
 return:
 	pop dx
 	pop bx
 	ret
-	
+
 print_char_16:
 	pusha
-	mov ah, 0x0E
+	mov ah, 0x0E		;print char service
 	mov al, dl
-	int 0x10
+	int 0x10		;video interrupt
 	popa
+	ret
+
+newline_16:
+	mov ah, 0x3		;get cursor data
+	int 0x10		;video interrupt
+	mov ah, 0x2		;set cursor location
+	mov dl, 0	
+	mov bh, 0
+	add dh, 1		;next line
+	int 0x10		;video interrupt
 	ret
